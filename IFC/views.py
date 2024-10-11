506a6c243de50bdd9d7c51e3a2f4cd06b97412d0
sadam-04 <asaich3@yahoo.com>
2024-10-11 18:46:47 -0400

    remove accreditation resource file

--- IFC/views.py
+++ IFC/views.py
@@ -1,4 +1,5 @@
 import os
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
