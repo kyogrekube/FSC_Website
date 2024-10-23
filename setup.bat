2124a11f19ab115158b774d518da15c51fec579f
sadam-04 <asaich3@yahoo.com>
2024-10-23 17:06:00 -0400

    final setup

--- setup.bat
+++ setup.bat
@@ -1,6 +1,13 @@
-@echo on
-py -3 -m venv .venv
+@echo off
+
+set venvpath=.venv
+set django__ver=5.1.1
+
+echo Setting up FSC Website project (Django %django__ver%)...
+echo -^>creating virtual environment: %venvpath%/
+py -3 -m venv %venvpath%
+echo -^>installing Django %django__ver% in %venvpath%/
 call .venv/scripts/activate
 py -m pip install --upgrade pip
 py -m pip install Django==5.1.1
-deactivate
+echo Done
