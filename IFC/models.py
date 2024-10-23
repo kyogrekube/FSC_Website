16fbc7eade68d6f9b88d6b72b9a8979a526e82fa
sadam-04 <asaich3@yahoo.com>
2024-10-23 14:22:10 -0400

    Revert "Revert "Merge branch 'main' into IFC/FSC-Rebrand""
    This reverts commit 10b8cae520f622804082ce1cbe1e2dc23b607103.

--- IFC/models.py
+++ IFC/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+# myapp/models.py
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+"""
+EXAMPLE OF HOW TO MAKE A TABLE:
+class table_name(models.Model):
+    name = models.CharField(max_length=100)   # This creates a VARCHAR column
+    age = models.IntegerField()               # This creates an INTEGER column
+    price = models.DecimalField(max_digits=10, decimal_places=2) # This creates a DECIMAL column
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'table_table'   # Explicitly set the table name
+"""
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=255)
+    letters = models.CharField(max_length=10)  # Assuming Greek letters are short
+    rush_chair = models.CharField(max_length=255)
+    president = models.CharField(max_length=255)
+    info = models.TextField(max_length=900)  # A 900 character info blob
+    chapter_size = models.PositiveIntegerField()  # Non-negative integer
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        chapter_slug = self.slug
+        chapter_slug = chapter_slug.replace('-', '')
+        return f'/chapters/{chapter_slug}'  # Use slug in the URL
+
+    def __str__(self):
+        return self.name
