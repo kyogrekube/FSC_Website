0614fa87ca63ac15625e60213e63332390cc5452
sadam-04 <asaich3@yahoo.com>
2024-10-23 16:13:59 -0400

    setup v2

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
-@ECHO OFF
+@echo on
 py -3 -m venv .venv
-.venv/scripts/activate
+call .venv/scripts/activate
 py -m pip install --upgrade pip
 py -m pip install Django==5.1.1
-code .
+echo "HELOOOOOOOOOO"
+call code .
