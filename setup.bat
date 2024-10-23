640cda4e6f51b292e5e62277eacfa9ec68fb20ad
sadam-04 <asaich3@yahoo.com>
2024-10-23 17:44:35 -0400

    set py interpreter to venv

--- setup.bat
+++ setup.bat
@@ -4,10 +4,16 @@ set venvpath=.venv
 set django__ver=5.1.1
 
 echo Setting up FSC Website project (Django %django__ver%)...
-echo -^>creating virtual environment: %venvpath%/
-py -3 -m venv %venvpath%
-echo -^>installing Django %django__ver% in %venvpath%/
-call .venv/scripts/activate
+echo Creating virtual environment: %venvpath%/
+py -3 -m venv %venvpath% >nul
+echo Installing Django %django__ver% in %venvpath%/
+call .venv/scripts/activate >nul
 py -m pip install --upgrade pip
 py -m pip install Django==5.1.1
+echo Setting python interpreter to %venvpath%/Scripts/activate
+(
+echo {.
+echo     "python.defaultInterpreterPath": ".\%venvpath%\Scripts\python.exe".
+echo }.
+) >> .vscode/settings.json
 echo Done
