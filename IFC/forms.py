5475cd0652f19e682d7e719594e5774af95e64a7
sadam-04 <asaich3@yahoo.com>
2024-10-25 18:09:45 -0400

    Basic system for LIVE one-time project setup tasks
    LIVE tasks are those that must be performed within Django or while Django is active.
    
    Example: batch adding database entries

--- IFC/forms.py
+++ IFC/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import Chapter
-
+from django.contrib.auth.models import User
 
 class ChapterForm(forms.ModelForm):
     class Meta:
         model = Chapter
         fields = ['name', 'letters', 'rush_chair', 'president', 'info', 'chapter_size']
+
+class RegisterForm(forms.Form):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_conf']
+
+    def save(self, commit=True):
+        user = super()
+
+    password = forms.CharField
--- setup.bat
+++ setup.bat
@@ -10,4 +10,7 @@ echo Installing Django %django__ver% in %venvpath%/
 call .venv/scripts/activate >nul
 py -m pip install --upgrade pip
 py -m pip install Django==5.1.1
+echo Performing live project init tasks
+%venvpath%/Scripts/python.exe manage.py project_init
 echo Done
+echo 
