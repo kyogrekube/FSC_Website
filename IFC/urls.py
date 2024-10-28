aa661328752b8ba3a67644e100736bc3856e3a2f
sadam-04 <asaich3@yahoo.com>
2024-10-28 06:46:34 -0400

    add slashes to urls, back button on chapter detail page

--- IFC/urls.py
+++ IFC/urls.py
@@ -27,21 +27,21 @@ urlpatterns = [
 
     # IFC app URLs
     path("", views.homepage, name="home"),
-    path("documents", views.documents, name="documents"),
-    path("calendar", views.calendar, name="calendar"),
-    path("leadership", views.leadership, name="leadership"),
-    path("recruitment", views.recruitment, name="recruitment"),
-    path("fall", views.fall, name="fall"),
-    path("spring", views.spring, name="spring"),
-    path("event-schedule", views.eventSchedule, name="event-schedule"),
+    path("documents/", views.documents, name="documents"),
+    path("calendar/", views.calendar, name="calendar"),
+    path("leadership/", views.leadership, name="leadership"),
+    path("recruitment/", views.recruitment, name="recruitment"),
+    path("fall/", views.fall, name="fall"),
+    path("spring/", views.spring, name="spring"),
+    path("event-schedule/", views.eventSchedule, name="event-schedule"),
 
     path("chapters/", views.ourChapters, name="chapters"),
     path('chapters/<slug:chapter_name>/', views.chapter_detail, name="chapter_detail"),
-    path("chapters/<slug:chapter_name>/edit", views.edit_chapter, name="edit_chapter"),
+    path("chapters/<slug:chapter_name>/edit/", views.edit_chapter, name="edit_chapter"),
 
     path('selectChapter', views.select_chapter, name="select_chapter"),
 
-    path('login', views.user_login, name="user_login"),
-    path('signup', views.user_signup, name="user_signup"),
-    path('logout', views.user_logout, name="user_logout"),
+    path('login/', views.user_login, name="user_login"),
+    path('signup/', views.user_signup, name="user_signup"),
+    path('logout/', views.user_logout, name="user_logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
