3ffa323c44afdaa712ecd0cd59b6c1ed5e3ed58d
sadam-04 <asaich3@yahoo.com>
2024-10-29 20:54:10 -0400

    Error handling and displaying

--- IFC/forms.py
+++ IFC/forms.py
@@ -11,7 +11,7 @@ class ChapterForm(forms.ModelForm):
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ['username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
